9b2ed879c064fd7a6e57dc8f842ae1ab13bdc33f
dikalee7 <51206302+dikalee7@users.noreply.github.com>
2023-05-12 15:52:32 +0900

    Update MainContent.js

--- src/domains/main/components/MainContent.js
+++ src/domains/main/components/MainContent.js
@@ -3,7 +3,7 @@ import { Button, Card } from 'react-bootstrap';
 
 const MainContent = ({ ctInfo, fnGoPage }) => {
   return (
-    <Card bg='secondary' text='white' style={{ marginTop: '15px' }}>
+    <Card style={{ marginTop: '15px' }}>
       {ctInfo.title ? <Card.Header>{ctInfo.title}</Card.Header> : ''}
 
       <Card.Body>
