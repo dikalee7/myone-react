e9a8a0017c7919060b4e8823f636269f20442872
dikalee7 <dikalee7@gmail.com>
2023-05-29 20:37:19 +0900

    수정

--- src/domains/benf/views/PubServiceDetail.js
+++ src/domains/benf/views/PubServiceDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import WithModal from 'components/modal/WithModal';
 const PubServiceDetail = ({ getParams, fullscreen, handleClose }) => {
   const { params, show } = getParams();
@@ -13,6 +14,10 @@ const PubServiceDetail = ({ getParams, fullscreen, handleClose }) => {
         추후 퍼블 적용 예정
         {params.data && <pre>{JSON.stringify(params.data[0], null, 2)}</pre>}
       </Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary'>Close</Button>
+        <Button variant='primary'>Save changes</Button>
+      </Modal.Footer>
     </Modal>
   );
 };
